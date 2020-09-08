8f5cc98c08aafa05946bea115c5e7db0eea98725
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2020-09-08 12:13:23 +0900

    A

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import StartPage from './pages/StartPage/StartPage';
+
+import './common.scss';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
-      {/* <Route path="/login" component={LoginPage} /> */}
+      <Route path="/start" component={StartPage} />
       <Redirect path="*" to="/" />
     </Switch>
   </BrowserRouter>
